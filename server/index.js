104cf1862c904c0015d88c3664a5c876e22d3832
Sand-Pigeon <skadbergjosh@gmail.com>
2022-03-13 20:23:06 -0500

    I hate git

--- server/index.js
+++ server/index.js
@@ -14,12 +14,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://deved:rhino11@cluster0.s5o98.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-// mongoose.set('useFindAndModify', false);
-mongoose.connect(CONNECTION_URL).then(()=>{console.log('...')})
+mongoose.set('useFindAndModify', false);
