7896f95995890c0b32b51e830a0c1a41d2271ad4
Sand-Pigeon <skadbergjosh@gmail.com>
2022-03-14 20:12:00 -0500

    Finished Auth Middleware

---
+++ client/src/actions/auth.js
@@ -0,0 +1,23 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+
+import * as api from '../api/index.js';
+
+export const signin = (formData, history) => async (dispatch) => {
+    try {
+        // log in the user..
+
+        history.push('/')
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const signup = (formData, history) => async (dispatch) => {
+    try {
+        // sign up the user..
+
+        history.push('/')
+    } catch (error) {
+        console.log(error);
+    }
+}
